7e06cdde4fa2b4a3658938ee8bca7e44d8b7521f
Ishita <ishitasanap5@gmail.com>
2026-04-03 10:59:04 +0530

    ready to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,7 +34,8 @@ if (process.env.NODE_ENV === 'production') {
   const clientBuildPath = path.join(__dirname, '../frontend/dist'); 
   app.use(express.static(clientBuildPath));
 
-  app.get('*', (req, res) => {
+  // Express 5: use '/*' instead of '*'
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(clientBuildPath, 'index.html'));
   });
 }
